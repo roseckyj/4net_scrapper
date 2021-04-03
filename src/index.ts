5c3e0054295fc42d37f07608886969048995377d
roseckyj <rosecky.jonas@centrum.cz>
2021-04-03 03:48:50 +0200

    Updated package.json

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import { FourNetScrapper } from "./FourNetScrapper";
 
 dotenv.config();
 
-const EPG_FILENAME = __dirname + "/epg/all.xml"
-const EPG_TMP_FILENAME = __dirname + "/epg/tmp.xml"
+const EPG_FILENAME = __dirname + "../epg/all.xml"
+const EPG_TMP_FILENAME = __dirname + "../epg/tmp.xml"
 
 const scrapper = new FourNetScrapper(process.env.API_URL, process.env.TOKEN, "cs");
 const app = express();
